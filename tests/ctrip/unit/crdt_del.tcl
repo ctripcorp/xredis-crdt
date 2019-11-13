078e5cd2d9193d8043edccf93337aba4925543f5
NickNYU <cz739@nyu.edu>
2019-11-13 18:03:45 +0800

    make crdt.set tests align with crdt.so

--- tests/ctrip/unit/crdt_del.tcl
+++ tests/ctrip/unit/crdt_del.tcl
@@ -16,7 +16,7 @@ start_server {tags {"crdt-del"} overrides {crdt-gid 1} config {crdt.conf} module
     test {"[crdt_del.tcl]concurrent crdt delete - 1"} {
         r set key-del-3 val
         after 1
-        r CRDT.SET key-del-3 val2 3 [expr [clock milliseconds] - 10]  "1:10;2:99;3:100"
+        r CRDT.SET key-del-3 val2 3 [expr [clock milliseconds] - 10]  "1:10;2:99;3:100" 10000
         r CRDT.DEL_REG key-del-3 2 [clock milliseconds] "1:30;2:100;3:100"
         r get key-del-3
     } {}
@@ -26,18 +26,18 @@ start_server {tags {"crdt-del"} overrides {crdt-gid 1} config {crdt.conf} module
         after 1
         r CRDT.DEL_REG key-del-4 2 [clock milliseconds] "1:30;2:100;3:100"
         after 1
-        r CRDT.SET key-del-4 val2 3 [expr [clock milliseconds] - 10]  "1:10;2:99;3:100"
+        r CRDT.SET key-del-4 val2 3 [expr [clock milliseconds] - 10]  "1:10;2:99;3:100"  10000
         r get key-del-4
     } {}
 
     test {"[crdt_del.tcl]concurrent crdt delete - 3"} {
         r CRDT.DEL_REG key-del-5 2 [clock milliseconds] "1:30;2:100;3:100"
-        r CRDT.SET key-del-5 val2 3 [expr [clock milliseconds] - 10]  "1:10;2:99;3:100"
+        r CRDT.SET key-del-5 val2 3 [expr [clock milliseconds] - 10]  "1:10;2:99;3:100"  10000
         r get key-del-5
     } {}
 
     test {"[crdt_del.tcl]concurrent crdt delete - 4"} {
-        r CRDT.SET key-del-6 val2 1 [expr [clock milliseconds] - 10]  "1:10"
+        r CRDT.SET key-del-6 val2 1 [expr [clock milliseconds] - 10]  "1:10"  10000
         r CRDT.DEL_REG key-del-6 2 [clock milliseconds] "2:100"
         r get key-del-6
     } {val2}
