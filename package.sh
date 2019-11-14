beea6b857bfc43ef5afd431cc3ae162c6307c307
NickNYU <cz739@nyu.edu>
2019-11-14 13:31:28 +0800

    make app to xredis-crdt and version to 1.0.2

--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
-APP=xredis
-Version=2.0.1
+APP=xredis-crdt
+Version=1.0.2
 Package=$APP-$Version
 
 find . -name ctrip.h -exec sed -i "s/XREDIS_VERSION \"\(.*\)\"/XREDIS_VERSION \"$Version\"/" {} \;
