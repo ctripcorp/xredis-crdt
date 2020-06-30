1e3406774258c684d489440f6a2368658dc33394
Chen <cz739@nyu.edu>
2020-06-30 16:23:02 +0800

    Feature/auto ci (#61)
    * temporary save
    
    * [ut-change]all tests passed

--- tests/support/server.tcl
+++ tests/support/server.tcl
@@ -195,6 +195,12 @@ proc start_redis_server {server options {code undefined}} {
     if {$module ne ""} {
         set module_path [exec pwd]
         append module_path "/tests/assets/"
+        set uname_S [exec uname -s]
+        if {$uname_S eq "Darwin"} {
+            append module_path "mac/"
+        } elseif {$uname_S eq "Linux"} {
+            append module_path "linux/"
+        }
         append module_path $module
         dict set config loadmodule $module_path
     }
