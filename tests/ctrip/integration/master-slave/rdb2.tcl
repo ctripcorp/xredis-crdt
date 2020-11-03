796c06f3ab0bbe2b6ae403dfa74e116ab143f19e
qiongtubao <810776886@qq.com>
2020-11-03 14:49:47 +0800

    add randomKey and randomValue test

--- tests/ctrip/integration/master-slave/rdb2.tcl
+++ tests/ctrip/integration/master-slave/rdb2.tcl
@@ -116,7 +116,7 @@ proc save {add check server_path dl} {
     }
 }
 array set adds ""
-
+set set_argvs {}
 set adds(0) {
     $redis set key value
     $redis set rc1 10
@@ -127,8 +127,16 @@ set adds(0) {
     $redis incrbyfloat rc4 1.2
     for {set i 0} {$i < 256} {incr i} {
         set a [i2b $i] 
-        $redis set a a 
-        $redis hset hashb a a
+        $redis set $a $a 
+        $redis hset hash_binary $a $a
+    }
+    for {set i 0} {$i < 256} {incr i} {
+        set argv {}
+        lappend argv [randomKey] 
+        lappend argv [randomValue] 
+        lappend set_argvs $argv
+        $redis set [lindex $argv 0] [lindex $argv 1]
+        $redis hset hash_random [lindex $argv 0] [lindex $argv 1]
     }
 }
 set checks(0) {
@@ -139,8 +147,13 @@ set checks(0) {
     assert_equal [$redis get rc4] 2.30000000000000000
     for {set i 0} {$i < 256} {incr i} {
         set a [i2b $i] 
-        assert_equal [$redis get a] a 
-        assert_equal [$redis hget hashb a] a
+        assert_equal [$redis get $a] $a 
+        assert_equal [$redis hget hash_binary $a] $a
+    }
+    for {set i 0} {$i < 256} {incr i} {
+        set argv  [lindex set_argvs $i]
+        assert_equal [$redis get [lindex $argv 0]] [lindex $argv 1]
+        assert_equal [$redis hget hash_random [lindex $argv 0]] [lindex $argv 1]
     }
 }
 set adds(1) {
