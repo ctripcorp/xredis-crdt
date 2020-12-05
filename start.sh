7cf39d4e203c8063aec42846f41a816c2554c0a5
qiongtubao <810776886@qq.com>
2020-12-05 18:02:42 +0800

    change start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 #only mac 
 cp ./tests/assets/mac/crdt.so ./
-./src/redis-server --crdt-gid default 1 --loadmodule ./crdt.so --port 7379 --logfile peer.log --daemonize yes 
+./src/redis-server --crdt-gid default 1 --loadmodule ./crdt.so --port 7379 --logfile master.log --daemonize yes 
 ./src/redis-server --crdt-gid default 2 --loadmodule ./crdt.so --port 7579 --logfile peer.log --daemonize yes 
 ./src/redis-cli -p 7379 peerof 2 127.0.0.1 7579
 ./src/redis-cli -p 7579 peerof 1 127.0.0.1 7379
