08ff1cf5d95f5f57ddcfcbd1b983347e78cf78bc
qiongtubao <810776886@qq.com>
2022-04-01 14:09:08 +0800

    [hotfix]get value unexpire when merge tombstone (#147)
    Co-authored-by: 周国栋 <zhouguodong@zhouguodongdeMacBook-Pro.local>

--- tests/ctrip/integration/master-slave/rdb2.tcl
+++ tests/ctrip/integration/master-slave/rdb2.tcl
@@ -144,7 +144,7 @@ set checks(0) {
     assert_equal [$redis get rc1] 10
     assert_equal [$redis get rc2] 1
     assert_equal [$redis get rc3] 6
-    assert_equal [$redis get rc4] 2.3
+    assert {abs([$redis get rc4] - 2.3) < 0.001}
 }
 set adds(1) {
     $redis hset hash k1 v1 k2 v2
