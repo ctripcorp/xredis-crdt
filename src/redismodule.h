890a2861d887eed665e93cec3a51d708394bb2c6
dong <810776886@qq.com>
2023-02-09 22:25:16 +0800

    hotfix statck overflow (counter type)

--- src/redismodule.h
+++ src/redismodule.h
@@ -149,7 +149,7 @@ typedef struct RedisModuleTypeMethods {
 
 #define REDISMODULE_API_FUNC(x) (*x)
 
-
+void *REDISMODULE_API_FUNC(RedisModule_GetSharedBuffer)(size_t bytes);
 void *REDISMODULE_API_FUNC(RedisModule_Alloc)(size_t bytes);
 void *REDISMODULE_API_FUNC(RedisModule_Realloc)(void *ptr, size_t bytes);
 void REDISMODULE_API_FUNC(RedisModule_Free)(void *ptr);
@@ -332,6 +332,7 @@ static int RedisModule_Init(RedisModuleCtx *ctx, const char *name, int ver, int 
 static int RedisModule_Init(RedisModuleCtx *ctx, const char *name, int ver, int apiver) {
     void *getapifuncptr = ((void**)ctx)[0];
     RedisModule_GetApi = (int (*)(const char *, void *)) (unsigned long)getapifuncptr;
+    REDISMODULE_GET_API(GetSharedBuffer);
     REDISMODULE_GET_API(Alloc);
     REDISMODULE_GET_API(ModuleMemory);
     REDISMODULE_GET_API(UsedMemory);
