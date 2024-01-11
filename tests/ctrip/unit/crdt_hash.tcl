73fa40cef8a5029c6d54b8cb24d0d1840e379947
gd.zhou <gd.zhou@trip.com>
2024-01-11 14:19:34 +0800

    hotfix hash null tombstonesize gc

--- tests/ctrip/unit/crdt_hash.tcl
+++ tests/ctrip/unit/crdt_hash.tcl
@@ -553,3 +553,51 @@ start_server { tags {"repl"} config {crdt.conf} overrides {crdt-gid 1 repl-diskl
 }
 
 
+
+start_server {tags {"repl"} overrides {crdt-gid 1} module {crdt.so} } {
+    set master [srv 0 client]
+    set master_host [srv 0 host]
+    set master_port [srv 0 port]
+    set master_log [srv 0 stdout]
+    $master config set hz 100
+    
+    start_server {tags {"repl"} overrides {crdt-gid 2} module {crdt.so} } {
+        set peer [srv 0 client]
+        set peer_host [srv 0 host]
+        set peer_port [srv 0 port]
+        set peer_log [srv 0 stdout]
+        $peer config set hz 100
+        $peer config crdt.set repl-timeout 120
+        $peer peerof 1 $master_host $master_port
+        $master peerof 2 $peer_host $peer_port
+        wait_for_peer_sync $peer 
+        wait_for_peer_sync $master 
+        $master hdel h k
+        if {[$master tombstonesize] == 1} {
+            set try 10
+            while  {$try > 0} {
+                if {[$master tombstonesize] == 0} {
+                    break;
+                }
+                after 500
+                set try [expr {$try-1}]
+            }
+            assert {$try > 0}
+        }
+        
+
+        $master hset h k v 
+        $master hdel h k1 
+        if {[$master tombstonesize] == 1} {
+            set try 10
+            while  {$try > 0} {
+                if {[$master tombstonesize] == 0} {
+                    break;
+                }
+                after 500
+                set try [expr {$try-1}]
+            }
+            assert {$try > 0}
+        }
+    }
+}
